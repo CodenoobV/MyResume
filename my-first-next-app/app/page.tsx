95389e5c5d938adb5cb6ab78fdf329d8a8526663
CodenoobV <lpvinh2k4@gmail.com>
2024-12-18 21:36:49 +1100

    First Creation of Next.js App

---
+++ my-first-next-app/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+     <main>
+      <h1>Hello World!</h1>
+      <h2>Current Attempt of Using Next.js</h2>
+
+      <Link href ="/about">About</Link>
+     </main>
+  );
+}
+ 

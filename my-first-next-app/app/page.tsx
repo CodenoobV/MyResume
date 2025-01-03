f26013f25c15d7ec44d3b0ebfb5a1a55f98f3fdb
CodenoobV <lpvinh2k4@gmail.com>
2025-01-03 20:01:36 +1100

    Fixed Some Issues

--- my-first-next-app/app/page.tsx
+++ my-first-next-app/app/page.tsx
@@ -1,12 +1,15 @@
-// import Image from "next/image";
-// import Link from "next/link";
+import Link from "next/link";
 
 export default function Home() {
   return (
-     <main>
-      <h1>Hello World!</h1>
-      <h2>Current Attempt of Using Next.js</h2>
-     </main>
+    <div className="App">
+      <h1 className="text-3xl fond-bold">
+        WELCOME
+      </h1>
+      <h2 className="text-2xl mt-4">
+        This Is My First Actual Next.JS App
+      </h2>
+      <Link className="bg-gray-700 text-white px-4 py-4 mt-4 font-bold rounded-lg" href="/todo"> Go To Todo App</Link>
+    </div>
   );
 }
- 

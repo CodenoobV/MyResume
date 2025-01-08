ce8f7403aaa2852f3cd8221ecb051e9d65e43a73
CodenoobV <lpvinh2k4@gmail.com>
2025-01-08 16:35:50 +1100

    I messed stuffs up

---
+++ my-first-next-app/app/resume/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import Papa from 'papaparse';
+
+interface unit {
+    year: number;
+    code: string;
+    unitName: string;
+}
+
+const ResumePage = () => {
+    const [units, setUnits] = useState<unit[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/data/education.csv');
+            const reader = response.body?.getReader();
+            const result = await reader?.read();
+            const decoder = new TextDecoder('utf-8');
+            const csv = decoder.decode(result?.value);
+            const parsedData = Papa.parse<unit>(csv, { header: true });
+            setUnits(parsedData.data);
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>This is my resume</p>
+            <ul>
+                {units.map((unit, index) => (
+                    <li key={index}>
+                        <h2>{unit.year} - {unit.code}</h2>
+                        <p>{unit.unitName}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ResumePage;
